82b0a9f407f487cd297a3fde76335ca7e35079a3
ItsRazyHero <58303566+ItsRazyHero@users.noreply.github.com>
2021-08-25 14:03:48 +0300

    Update mainx.py
    add answers list

--- mainx.py
+++ mainx.py
@@ -14,7 +14,8 @@ html1 = BS(r1.content, "html.parser")
 for el in html.select('#saying'):
     title = el.select('td')
 aboba = str(title)[5:str(title).find('.')+1]
-
+#list for answers 8ball
+answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes – definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes Signs point to yes', 'Reply hazy', 'try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Dont count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
 #WorldLevel = 0
 #WorldPoints = 0
 #def isLevelReached():
